478558efa058b71094f2231f9c2ad8df96012dad
open-schnick <dev2@filefighter.de>
2023-11-11 14:28:53 +0100

    chore: expose assert_that

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-#![allow(dead_code, unused_variables)]
 use assertion_builder::AssertionBuilder;
 
 mod assertion_builder;
 mod assertions;
 
-fn assert_that<AssertedType>(value: AssertedType) -> AssertionBuilder<AssertedType> {
+pub fn assert_that<AssertedType>(value: AssertedType) -> AssertionBuilder<AssertedType> {
     AssertionBuilder { value }
 }
