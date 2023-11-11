0b6f2b804f9fc6a9474a8441d07adc389d25d6b8
open-schnick <dev2@filefighter.de>
2023-11-11 13:14:25 +0100

    feat: init with equals and try_equals

---
+++ src/lib.rs
@@ -0,0 +1,143 @@
+#![allow(dead_code, unused_variables)]
+
+fn assert_that<AssertedType>(value: AssertedType) -> AssertionBuilder<AssertedType> {
+    AssertionBuilder { value }
+}
+
+pub struct AssertionBuilder<AssertedType> {
+    value: AssertedType,
+}
+
+impl<AssertedType> AssertionBuilder<AssertedType>
+where
+    AssertedType: PartialEq + std::fmt::Debug,
+{
+    // FIXME: the type inference is bad as i32 does not implement Into<u16>
+    pub fn equals(&self, expected: impl Into<AssertedType>) {
+        use pretty_assertions::assert_eq;
+
+        let expected: AssertedType = expected.into();
+        assert_eq!(self.value, expected);
+    }
+
+    // IDEA: can one combine try_equals with equals?
+    pub fn try_equals<T>(&self, expected: T)
+    where
+        T: TryInto<AssertedType>,
+        <T as TryInto<AssertedType>>::Error: std::fmt::Debug,
+    {
+        use pretty_assertions::assert_eq;
+
+        let expected: AssertedType = expected.try_into().unwrap();
+        assert_eq!(self.value, expected);
+    }
+}
+
+#[cfg(test)]
+
+mod equals {
+    use super::*;
+
+    mod with_numbers {
+        use super::*;
+
+        mod u8 {
+            use super::*;
+
+            #[test]
+            fn min() {
+                assert_that(u8::MIN).equals(0);
+            }
+
+            #[test]
+            fn max() {
+                assert_that(u8::MAX).equals(255);
+            }
+
+            #[test]
+            fn random_value() {
+                let random_value: u8 = 123;
+                assert_that(random_value).equals(123);
+            }
+
+            #[test]
+            #[should_panic = "assertion failed: `(left == right)`"]
+            fn fails() {
+                let random_value: u8 = 123;
+                assert_that(random_value).equals(42);
+            }
+        }
+
+        mod u16 {
+            use super::*;
+
+            #[test]
+            fn min() {
+                assert_that(u16::MIN).equals(0u16);
+            }
+
+            #[test]
+            fn max() {
+                assert_that(u16::MAX).equals(65535u16);
+            }
+
+            #[test]
+            fn random_value() {
+                let random_value: u16 = 321;
+                assert_that(random_value).equals(321u16);
+            }
+
+            #[test]
+            fn can_be_compared_to_u8() {
+                let random_value: u16 = 123;
+                assert_that(random_value).equals(123u8);
+            }
+
+            #[test]
+            #[should_panic = "assertion failed: `(left == right)`"]
+            fn fails() {
+                let random_value: u16 = 123;
+                assert_that(random_value).equals(321u16);
+            }
+        }
+
+        mod u32 {
+            use super::*;
+
+            #[test]
+            fn min() {
+                assert_that(u32::MIN).equals(0u32);
+            }
+
+            #[test]
+            fn max() {
+                assert_that(u32::MAX).equals(4294967295u32);
+            }
+
+            #[test]
+            fn random_value() {
+                let random_value: u32 = 321;
+                assert_that(random_value).equals(321u32);
+            }
+
+            #[test]
+            fn can_be_compared_to_u8() {
+                let random_value: u32 = 123;
+                assert_that(random_value).equals(123u8);
+            }
+
+            #[test]
+            fn can_be_compared_to_u16() {
+                let random_value: u32 = 123;
+                assert_that(random_value).equals(123u16);
+            }
+
+            #[test]
+            #[should_panic = "assertion failed: `(left == right)`"]
+            fn fails() {
+                let random_value: u32 = 123;
+                assert_that(random_value).equals(321u32);
+            }
+        }
+    }
+}
