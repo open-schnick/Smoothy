1c8df50f14dd4940d57d5e97ae40444a1933fb85
open-schnick <dev2@filefighter.de>
2023-11-11 14:14:38 +0100

    test: add missing equal tests for numbers

--- src/assertions/equals.rs
+++ src/assertions/equals.rs
@@ -115,4 +115,356 @@ mod with_numbers {
             assert_that(random_value).equals(321u32);
         }
     }
+
+    mod u64 {
+        use super::*;
+
+        #[test]
+        fn min() {
+            assert_that(u64::MIN).equals(0u64);
+        }
+
+        #[test]
+        fn max() {
+            assert_that(u64::MAX).equals(18446744073709551615u64);
+        }
+
+        #[test]
+        fn random_value() {
+            let random_value: u64 = 321;
+            assert_that(random_value).equals(321u64);
+        }
+
+        #[test]
+        fn can_be_compared_to_u8() {
+            let random_value: u64 = 123;
+            assert_that(random_value).equals(123u8);
+        }
+
+        #[test]
+        fn can_be_compared_to_u16() {
+            let random_value: u64 = 123;
+            assert_that(random_value).equals(123u16);
+        }
+
+        #[test]
+        fn can_be_compared_to_u32() {
+            let random_value: u64 = 123;
+            assert_that(random_value).equals(123u32);
+        }
+
+        #[test]
+        #[should_panic = "assertion failed: `(left == right)`"]
+        fn fails() {
+            let random_value: u64 = 123;
+            assert_that(random_value).equals(321u64);
+        }
+    }
+
+    mod u128 {
+        use super::*;
+
+        #[test]
+        fn min() {
+            assert_that(u128::MIN).equals(0u128);
+        }
+
+        #[test]
+        fn max() {
+            assert_that(u128::MAX).equals(340282366920938463463374607431768211455u128);
+        }
+
+        #[test]
+        fn random_value() {
+            let random_value: u128 = 321;
+            assert_that(random_value).equals(321u128);
+        }
+
+        #[test]
+        fn can_be_compared_to_u8() {
+            let random_value: u128 = 123;
+            assert_that(random_value).equals(123u8);
+        }
+
+        #[test]
+        fn can_be_compared_to_u16() {
+            let random_value: u128 = 123;
+            assert_that(random_value).equals(123u16);
+        }
+
+        #[test]
+        fn can_be_compared_to_u32() {
+            let random_value: u128 = 123;
+            assert_that(random_value).equals(123u32);
+        }
+
+        #[test]
+        fn can_be_compared_to_u64() {
+            let random_value: u128 = 123;
+            assert_that(random_value).equals(123u64);
+        }
+
+        #[test]
+        #[should_panic = "assertion failed: `(left == right)`"]
+        fn fails() {
+            let random_value: u128 = 123;
+            assert_that(random_value).equals(321u128);
+        }
+    }
+
+    mod i8 {
+        use super::*;
+
+        #[test]
+        fn min() {
+            assert_that(i8::MIN).equals(-128);
+        }
+
+        #[test]
+        fn max() {
+            assert_that(i8::MAX).equals(127);
+        }
+
+        #[test]
+        fn random_value() {
+            let random_value: i8 = 123;
+            assert_that(random_value).equals(123);
+        }
+
+        #[test]
+        #[should_panic = "assertion failed: `(left == right)`"]
+        fn fails() {
+            let random_value: i8 = 123;
+            assert_that(random_value).equals(42);
+        }
+    }
+
+    mod i16 {
+        use super::*;
+
+        #[test]
+        fn min() {
+            assert_that(i16::MIN).equals(-32768i16);
+        }
+
+        #[test]
+        fn max() {
+            assert_that(i16::MAX).equals(32767i16);
+        }
+
+        #[test]
+        fn random_value() {
+            let random_value: i16 = 321;
+            assert_that(random_value).equals(321i16);
+        }
+
+        #[test]
+        fn can_be_compared_to_i8() {
+            let random_value: i16 = 123;
+            assert_that(random_value).equals(123i8);
+        }
+
+        #[test]
+        #[should_panic = "assertion failed: `(left == right)`"]
+        fn fails() {
+            let random_value: i16 = 123;
+            assert_that(random_value).equals(321i16);
+        }
+    }
+
+    mod i32 {
+        use super::*;
+
+        #[test]
+        fn min() {
+            assert_that(i32::MIN).equals(-2147483648i32);
+        }
+
+        #[test]
+        fn max() {
+            assert_that(i32::MAX).equals(2147483647i32);
+        }
+
+        #[test]
+        fn random_value() {
+            let random_value: i32 = 321;
+            assert_that(random_value).equals(321i32);
+        }
+
+        #[test]
+        fn can_be_compared_to_i8() {
+            let random_value: i32 = 123;
+            assert_that(random_value).equals(123i8);
+        }
+
+        #[test]
+        fn can_be_compared_to_i16() {
+            let random_value: i32 = 123;
+            assert_that(random_value).equals(123i16);
+        }
+
+        #[test]
+        #[should_panic = "assertion failed: `(left == right)`"]
+        fn fails() {
+            let random_value: u32 = 123;
+            assert_that(random_value).equals(321u32);
+        }
+    }
+
+    mod i64 {
+        use super::*;
+
+        #[test]
+        fn min() {
+            assert_that(i64::MIN).equals(-9223372036854775808i64);
+        }
+
+        #[test]
+        fn max() {
+            assert_that(i64::MAX).equals(9223372036854775807i64);
+        }
+
+        #[test]
+        fn random_value() {
+            let random_value: i64 = 321;
+            assert_that(random_value).equals(321i64);
+        }
+
+        #[test]
+        fn can_be_compared_to_i8() {
+            let random_value: i64 = 123;
+            assert_that(random_value).equals(123i8);
+        }
+
+        #[test]
+        fn can_be_compared_to_i16() {
+            let random_value: i64 = 123;
+            assert_that(random_value).equals(123i16);
+        }
+
+        #[test]
+        fn can_be_compared_to_i32() {
+            let random_value: i64 = 123;
+            assert_that(random_value).equals(123i32);
+        }
+
+        #[test]
+        #[should_panic = "assertion failed: `(left == right)`"]
+        fn fails() {
+            let random_value: i64 = 123;
+            assert_that(random_value).equals(321i64);
+        }
+    }
+
+    mod i128 {
+        use super::*;
+
+        #[test]
+        fn min() {
+            assert_that(i128::MIN).equals(-170141183460469231731687303715884105728i128);
+        }
+
+        #[test]
+        fn max() {
+            assert_that(i128::MAX).equals(170141183460469231731687303715884105727i128);
+        }
+
+        #[test]
+        fn random_value() {
+            let random_value: i128 = 321;
+            assert_that(random_value).equals(321i128);
+        }
+
+        #[test]
+        fn can_be_compared_to_i8() {
+            let random_value: i128 = 123;
+            assert_that(random_value).equals(123i8);
+        }
+
+        #[test]
+        fn can_be_compared_to_i16() {
+            let random_value: i128 = 123;
+            assert_that(random_value).equals(123i16);
+        }
+
+        #[test]
+        fn can_be_compared_to_i32() {
+            let random_value: i128 = 123;
+            assert_that(random_value).equals(123i32);
+        }
+
+        #[test]
+        fn can_be_compared_to_i64() {
+            let random_value: i128 = 123;
+            assert_that(random_value).equals(123i64);
+        }
+
+        #[test]
+        #[should_panic = "assertion failed: `(left == right)`"]
+        fn fails() {
+            let random_value: i128 = 123;
+            assert_that(random_value).equals(321i128);
+        }
+    }
+
+    mod f32 {
+        use super::*;
+
+        #[test]
+        fn min() {
+            assert_that(f32::MIN).equals(-3.4028235e38);
+        }
+
+        #[test]
+        fn max() {
+            assert_that(f32::MAX).equals(3.4028235e38);
+        }
+
+        #[test]
+        fn random_value() {
+            let random_value: f32 = 123.45;
+            assert_that(random_value).equals(123.45);
+        }
+
+        #[test]
+        #[should_panic = "assertion failed: `(left == right)`"]
+        fn fails() {
+            let random_value: f32 = 123.;
+            assert_that(random_value).equals(42.);
+        }
+    }
+
+    mod f64 {
+        use super::*;
+
+        #[test]
+        fn min() {
+            assert_that(f64::MIN).equals(-1.7976931348623157e308);
+        }
+
+        #[test]
+        fn max() {
+            assert_that(f64::MAX).equals(1.7976931348623157e308);
+        }
+
+        #[test]
+        fn random_value() {
+            let random_value: f64 = 123.45;
+            assert_that(random_value).equals(123.45);
+        }
+
+        #[test]
+        #[should_panic = "assertion failed: `(left == right)`"]
+        fn cannot_be_compared_to_f32() {
+            let random_value: f64 = 123.45;
+            assert_that(random_value).equals(123.45f32);
+        }
+
+        #[test]
+        #[should_panic = "assertion failed: `(left == right)`"]
+        fn fails() {
+            let random_value: f64 = 123.45;
+            assert_that(random_value).equals(321.01);
+        }
+    }
 }
