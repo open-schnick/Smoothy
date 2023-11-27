306d8ffe3c7982cf27118bf3fd95fc7134fbdb55
open-schnick <dev2@filefighter.de>
2023-11-27 16:01:46 +0100

    test: add missing test for to_string

---
+++ tests/to_string.rs
@@ -0,0 +1,37 @@
+use smoothy::*;
+use utils::TestStruct;
+
+#[test]
+fn succeeds_with_string() {
+    let asserter: BasicAsserter<String> = assert_that(String::from("Hello World!")).to_string();
+    asserter.equals("Hello World!");
+}
+
+#[test]
+fn succeeds_with_str() {
+    let asserter: BasicAsserter<String> = assert_that("Hello World!").to_string();
+    asserter.equals("Hello World!");
+}
+
+#[test]
+fn succeeds_with_number() {
+    let asserter: BasicAsserter<String> = assert_that(42).to_string();
+    asserter.equals("42");
+}
+
+#[test]
+fn succeeds_with_struct() {
+    let asserter: BasicAsserter<String> = assert_that(TestStruct::default()).to_string();
+    asserter.equals("TestStruct");
+}
+
+mod utils {
+    #[derive(Default)]
+    pub struct TestStruct;
+
+    impl ToString for TestStruct {
+        fn to_string(&self) -> String {
+            String::from("TestStruct")
+        }
+    }
+}
