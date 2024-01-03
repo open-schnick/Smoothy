758a7c57bd9766ac93494f30487ff2b9e34eb07d
open-schnick <dev2@filefighter.de>
2024-01-03 12:18:28 +0100

    test: add missing indices tests

---
+++ tests/indices.rs
@@ -0,0 +1,77 @@
+use smoothy::assert_that;
+
+mod first {
+    use super::*;
+
+    #[test]
+    fn succeeds() {
+        assert_that(vec!["Hello World!"])
+            .first()
+            .equals("Hello World!");
+    }
+
+    #[test]
+    #[should_panic = "assertion failed: `(Iterator has first item)`\n           found:  None"]
+    fn fails_with_empty_vec() {
+        assert_that::<Vec<&str>>(vec![])
+            .first()
+            .equals("Hello World!");
+    }
+}
+
+mod second {
+    use super::*;
+
+    #[test]
+    fn succeeds() {
+        assert_that(vec!["First", "Second"])
+            .second()
+            .equals("Second");
+    }
+
+    #[test]
+    #[should_panic = "assertion failed: `(Iterator has second item)`\n           found:  None"]
+    fn fails_with_empty_vec() {
+        assert_that::<Vec<&str>>(vec![])
+            .second()
+            .equals("Hello World!");
+    }
+}
+
+mod third {
+    use super::*;
+
+    #[test]
+    fn succeeds() {
+        assert_that(vec!["First", "Second", "Third"])
+            .third()
+            .equals("Third");
+    }
+
+    #[test]
+    #[should_panic = "assertion failed: `(Iterator has second item)`\n           found:  None"]
+    fn fails_with_empty_vec() {
+        assert_that::<Vec<&str>>(vec![])
+            .third()
+            .equals("Hello World!");
+    }
+}
+
+mod nth {
+    use super::*;
+
+    #[test]
+    fn succeeds() {
+        assert_that(vec!["First", "Second", "Third"])
+            .nth(2)
+            .equals("Third");
+    }
+
+    #[test]
+    #[should_panic = "assertion failed: `(Iterator has 2th item)`\n           found:  None"]
+    fn fails_with_empty_vec() {
+        assert_that::<Vec<&str>>(vec![])
+            .nth(2)
+            .equals("Hello World!");
+    }
+}
