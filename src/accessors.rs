9b42428157e879de74b471f0bca6a410af780d92
open-schnick <60597856+open-schnick@users.noreply.github.com>
2024-12-14 21:23:45 +0100

    refactor: move to_string into accessors

--- src/accessors.rs
+++ src/accessors.rs
@@ -45,3 +45,26 @@ impl<AssertedType> BasicAsserter<AssertedType> {
         assert_that(mapped)
     }
 }
+
+impl<AssertedType> BasicAsserter<AssertedType>
+where
+    AssertedType: ToString,
+{
+    /// Converts the assertable to a string for further assertions
+    ///
+    /// # Examples
+    /// ```
+    /// # use smoothy::prelude::*;
+    /// #
+    /// let asserter: BasicAsserter<String> = assert_that(42).to_string();
+    /// // further assertions
+    /// asserter.equals("42");
+    /// ```
+    #[track_caller]
+    #[must_use = "Transforming the asserted value does not assert anything"]
+    pub fn to_string(self) -> BasicAsserter<String> {
+        BasicAsserter {
+            value: self.value.to_string(),
+        }
+    }
+}
--- src/string.rs
+++ src/string.rs
@@ -1,4 +1,3 @@
 mod contains_string;
 mod is_matching;
 mod starts_with_string;
-mod to_string;
