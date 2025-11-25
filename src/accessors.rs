8c7735237a05ddee65e6b06f6803a6e8c9449793
open-schnick <open-schnick@web.de>
2025-11-25 21:47:43 +0100

    refactor!: remove deprecated map function
    BREAKING CHANGE: this may break downstream code which depends on the
    deprecated function

--- src/accessors.rs
+++ src/accessors.rs
@@ -1,51 +1,6 @@
 use crate::{assert_that, BasicAsserter};
 
 impl<AssertedType> BasicAsserter<AssertedType> {
-    /// Maps the inner value to an assertion of a different type
-    ///
-    /// This allows for a property access of inner values or functions
-    ///
-    /// # Examples
-    /// ```
-    /// # use smoothy::prelude::*;
-    /// struct Struct(pub String);
-    ///
-    /// assert_that(Struct("hello".to_string()))
-    ///     .map(|s| s.0)
-    ///     .equals("hello");
-    /// ```
-    ///
-    /// The mapping could be hidden by a trait
-    ///
-    /// ```rust
-    /// # use smoothy::prelude::*;
-    /// struct Struct(pub String);
-    ///
-    /// pub trait SmoothyExt {
-    ///     fn inner_value(self) -> BasicAsserter<String>;
-    /// }
-    ///
-    /// impl SmoothyExt for BasicAsserter<Struct> {
-    ///     fn inner_value(self) -> BasicAsserter<String> {
-    ///         self.map(|s| s.0)
-    ///     }
-    /// }
-    ///
-    /// assert_that(Struct("hello".to_string()))
-    ///     .inner_value()
-    ///     .equals("hello");
-    /// ```
-    #[track_caller]
-    #[must_use = "Mapping an assertion without second assertion does nothing"]
-    #[deprecated(since = "0.8.3", note = "Use `extract()` instead")]
-    pub fn map<NewAssertedType>(
-        self,
-        mapping: impl FnOnce(AssertedType) -> NewAssertedType,
-    ) -> BasicAsserter<NewAssertedType> {
-        let mapped = mapping(self.value);
-        assert_that(mapped)
-    }
-
     /// Extracts the value of an asserted value
     ///
     /// This allows for a property access of inner values or functions
